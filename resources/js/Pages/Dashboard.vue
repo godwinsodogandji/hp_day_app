615b3c698ab2db5fdb73c2604ad7e1429261a08c
Chirac Michoagan <michoaganchiracdona@gmail.com>
2024-11-08 19:41:36 +0100

    font icons

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -25,6 +25,17 @@ import { Head } from '@inertiajs/vue3';
                     </div>
                 </div>
             </div>
+            <template>
+  <div>
+    <h1 style="font-family: 'Charme', sans-serif;">Titre avec Charme</h1>
+    <p style="font-family: 'Amour', sans-serif;">Texte avec Amour</p>
+    <p style="font-family: 'Chalereux', sans-serif;">Texte avec Chalereux</p>
+    <p style="font-family: 'Romantique', sans-serif;">Texte avec Romantique</p>
+    <p style="font-family: 'Charmonman', sans-serif;">Texte avec Charmonman</p>
+    <p style="font-family: 'Querelle d\'amoureux', sans-serif;">Texte avec Querelle d'amoureux</p>
+    <p style="font-family: 'Aimé par le roi', sans-serif;">Texte avec Aimé par le roi</p>
+  </div>
+</template>
         </div>
     </AuthenticatedLayout>
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,11 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Charme&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="stylesheet" href="{{ asset('fonts/fonts.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
