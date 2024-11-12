034681e1bd256b7c11e15738e8a9c3164044e720
godwinsodogandji <jeansodogandji@gmail.com>
2024-11-12 18:36:50 +0100

    scroll enlevé dans app.blade

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head,Link } from "@inertiajs/vue3";
+import { Head } from "@inertiajs/vue3";
 import { ref, onMounted } from "vue";
 import lottie from "lottie-web";
 // État pour gérer l'ouverture/fermeture de l'aside
@@ -372,12 +372,11 @@ onMounted(() => {
                                     >{{ friend.birthday }}</span
                                 >
                                 <div class="flex mt-4 md:mt-6">
-                                    <Link
-                                        href="/mail"
+                                    <button
                                         class="bg-blue-500 text-white py-1 px-3 rounded hover:bg-blue-600"
                                     >
                                         Souhaiter
-                                    </Link>
+                                    </button>
                                 </div>
                             </div>
                         </div>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Charme&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('fonts/fonts.css') }}">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Charme&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('fonts/fonts.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <style>
+        html, body {
+            height: 100%; 
+            overflow: hidden; 
+            margin: 0; 
+        }
+    </style>
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+<body class="font-sans antialiased">
+    @inertia
+</body>
 </html>
