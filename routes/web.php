757b4e3c6ae6d5f6b6398cac1772e1457f58ab3c
godwinsodogandji <jeansodogandji@gmail.com>
2024-11-13 12:25:06 +0100

    commit effectué pour récupérer la branche Ange

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\BirthdayController;
+use App\Http\Controllers\SouhaiterJoyeuxAnniversaire;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 Route::get('/', function () {
